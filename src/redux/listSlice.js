eaca7dff7ccc2107edaa26b17119a81504de12b5
Bhanu-techy <bhanuprakashdevari@gmail.com>
2025-09-22 05:46:25 +0000

    solution

--- src/redux/listSlice.js
+++ src/redux/listSlice.js
@@ -1,24 +1,27 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 
-export const fetchData = createAsyncThunk('users/fetchUsers', async () => {
+export const fetchData = createAsyncThunk('todos/fetchData', async () => {
   const response = await fetch(
     'https://task-managment-production-3b91.up.railway.app/api/todos',
   )
   return response.json()
 })
 
-export const createTask = createAsyncThunk('user/fectchUser', async newTask => {
-  const options = {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(newTask),
-  }
-  const response = await fetch(
-    'https://task-managment-production-3b91.up.railway.app/api/todos',
-    options,
-  )
-  return response.json()
-})
+export const createTask = createAsyncThunk(
+  'todos/createTask',
+  async newTask => {
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(newTask),
+    }
+    const response = await fetch(
+      'https://task-managment-production-3b91.up.railway.app/api/todos',
+      options,
+    )
+    return response.json()
+  },
+)
 
 export const listSlice = createSlice({
   name: 'list',
@@ -28,15 +31,16 @@ export const listSlice = createSlice({
   reducers: {},
 
   extraReducers: builder => {
-    builder.addCase(createTask.fulfilled, (state, action) => {
-      state.data.push(action.payload)
-    })
     builder.addCase(fetchData.fulfilled, (state, action) => {
       state.data = action.payload
+    })
+    builder.addCase(createTask.fulfilled, (state, action) => {
+      state.data.push(action.payload)
     })
   },
 })
 
 export const {addTask} = listSlice.actions
 
-export default listSlice.reducer
+const listReducer = listSlice.reducer
+export default listReducer
